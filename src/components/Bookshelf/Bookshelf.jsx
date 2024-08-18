ac23122bcdd5bece1d2fc75bb9b2aebba5d843dc
Waleed Robertson <waleedrbrtsn1@email.com>
2024-08-18 13:28:13 -0400

    Working through lab

--- src/components/Bookshelf/Bookshelf.jsx
+++ src/components/Bookshelf/Bookshelf.jsx
@@ -19,23 +19,45 @@ const Bookshelf = () => {
   });
 
   const handleInputChange = (event) => {
-    setFormData({ ...formData, [event.target.name]: event.target.value });
+    const { name, value } = event.target;
+    const updatedNewBook = { ...newBook, [name]: value };
+    setNewBook(updatedNewBook);
   };
 
 
-  <div className="bookshelfDiv">
-    <div className="formDiv">
-      <h3>Add a Book</h3>
-      {/* Form will go here */}
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const newBooksList = [...books, newBook];
+    setBooks(newBooksList);
+    setNewBook({ title: '', author: '' });
+  };
+
+  return (
+    <div className="bookshelfDiv">
+      <div className="formDiv">
+        <h3>Add a Book</h3>
+        <input
+          type="text"
+          name="title"
+          placeholder="Title"
+          value={newBook.title}
+          onChange={handleInputChange}
+        />
+        <input
+          type="text"
+          id="author"
+          name="author"
+          placeholder="Author"
+          value={newBook.author}
+          onChange={handleInputChange}   
+
+        />
+        {/* Add submit button here (optional) */}
+      </div>
+      {/* Book cards will display here */}
     </div>
-    <div className="bookCardsDiv">{/* Book cards will display here */}</div>
-  </div>
+  );
 };
-
-
-
-
-
 
 
 export default Bookshelf;

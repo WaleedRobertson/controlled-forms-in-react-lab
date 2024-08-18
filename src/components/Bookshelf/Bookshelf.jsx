9be2d84aceabdc8800931a7d80dcbe09c3eb78d1
Waleed Robertson <waleedrbrtsn1@email.com>
2024-08-18 11:03:08 -0400

    Working through errors of first function of exercise

--- src/components/Bookshelf/Bookshelf.jsx
+++ src/components/Bookshelf/Bookshelf.jsx
@@ -1,11 +1,41 @@
 import { useState } from 'react';
 
-<div className="bookshelfDiv">
-  <div className="formDiv">
-    <h3>Add a Book</h3>
-    {/* Form will go here */}
+
+
+
+
+const Bookshelf = () => {
+  const [books, setBooks] = useState([
+    { title: 'Fourth Wing', author: 'Rebecca Yarros' },
+    { title: 'The Lion, the Witch and the Wardrobe', author: 'C.S. Lewis' },
+  ]);
+
+
+
+
+  const [newBook, setFormData] = useState({
+    title: '',
+    author: '',
+  });
+
+  const handleInputChange = (event) => {
+    setFormData({ ...formData, [event.target.name]: event.target.value });
+  };
+
+
+  <div className="bookshelfDiv">
+    <div className="formDiv">
+      <h3>Add a Book</h3>
+      {/* Form will go here */}
+    </div>
+    <div className="bookCardsDiv">{/* Book cards will display here */}</div>
   </div>
-  <div className="bookCardsDiv">{/* Book cards will display here */}</div>
-</div>
+};
+
+
+
+
+
+
 
 export default Bookshelf;
